8fd2ff942aaad20275e7c0beacfa944fff3f7d2f
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-09-27 21:15:49 -0500

    fixing createsuperusers

--- users/models.py
+++ users/models.py
@@ -7,22 +7,19 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=50, default="default_username", unique=True)
-    # email = models.EmailField(_('email address'), unique=True)
-    # fiels module
-    # is_student = models.BooleanField(default=False)
-    # is_teacher = models.BooleanField(default=False)
-    
-    # cvlac = models.CharField(max_length=200)
-    # fiels module
+    email = models.EmailField(_('email address'), unique=True)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = [username]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
     def __str__(self):
         return self.username
 
+    def save(self, *args, **kwargs):
+        self.username = self.email.split('@')[0]
+        super(CustomUser, self).save(*args, **kwargs)
 
 
 
