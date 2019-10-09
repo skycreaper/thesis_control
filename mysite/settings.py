9f562a2a2f1b777304eaf064f0b3d99ed4814f12
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-10-08 23:09:14 -0500

    Changes in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,16 +93,16 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'thesis_control',
-#         'USER': 'postgres',
-#         'PASSWORD': 'mypassword',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thesis_control',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
