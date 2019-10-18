ab768488e597a2fda0c0459368f70dcc34d6a7b0
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-10-17 22:48:57 -0500

    Update models: * Commentary * Documents

--- academic/models.py
+++ academic/models.py
@@ -76,15 +76,6 @@ class InvestigationLine(models.Model):
     def __str__(self):
         return self.name
 
-class ComentsThread(models.Model):
-    comment = models.CharField("comment", max_length=200)
-    author = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-
-class Document(models.Model):
-    document_file = models.FileField("file", upload_to=None, max_length=100)
-    title = models.CharField("title", max_length=50)
-    description = models.CharField("description", max_length=250)
-
 class ThesisState(models.Model):
     name = models.CharField("name", max_length=50)
     description = models.CharField("description", max_length=250)
@@ -99,6 +90,16 @@ class Thesis(models.Model):
     state = models.ForeignKey(ThesisState, on_delete=models.CASCADE, null=False)                # para workflow
     publication_date = models.DateTimeField('date publicated')
 
+class ComentsThread(models.Model):
+    comment = models.CharField("comment", max_length=200)
+    author = models.IntegerField("author")
+    thesis = models.ForeignKey(Thesis, on_delete=models.CASCADE, null=False)
+
+class Document(models.Model):
+    document_file = models.FileField("file", upload_to=None, max_length=100)
+    title = models.CharField("title", max_length=50)
+    description = models.CharField("description", max_length=250)
+    thesis = models.ForeignKey(Thesis, on_delete=models.CASCADE, null=False)
 
 class Advance(models.Model):
     thesis = models.ForeignKey(Thesis, on_delete=models.CASCADE)
