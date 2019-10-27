905c8a21d824af4c7f2c00e4f4d40293178ec844
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-10-26 21:44:22 -0500

    Fixing advance validation

--- academic/transactions.py
+++ academic/transactions.py
@@ -154,7 +154,7 @@ def UpdateTeacher(user_id, data, photo):
 def RegisterAdvance(data):
     thesis = Thesis.objects.get(pk=data["thesis"])
     actual_advance = sum(advance.percentage for advance in Advance.objects.filter(thesis=thesis))
-    if actual_advance + data["percentage"] <= 100:  
+    if actual_advance + int(data["percentage"]) <= 100:  
         advance = Advance(
             thesis=thesis,
             description=data["description"],
