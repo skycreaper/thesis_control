6df001c27ddbc29fde62f54a745a824475b9b5fb
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-11-24 14:39:32 -0500

    subprogram list by program and subprogram creation

--- programs/urls.py
+++ programs/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.conf.urls import url, include
 
-from .views import ProgramList, ProgramCreation, ProgramUpdate, SubProgramList
+from .views import ProgramList, ProgramCreation, ProgramUpdate, ProgramDetail, SubProgramList, SubProgramList, SubprogramCreation
 
 urlpatterns = [
     path('program_list', ProgramList.as_view(), name='program_list'),
     path('program_add', ProgramCreation.as_view(), name='program_add'),
     path('program_edit/<int:pk>', ProgramUpdate.as_view(), name='program_edit'),
+    path('program_detail/<int:pk>', ProgramDetail.as_view(), name='program_detail'),
+    path('subprogram_by_program_list/<int:pk>', SubProgramList.as_view(), name='subprogram_by_program'),
     path('sub_program_list', SubProgramList.as_view(), name='sub_program_list'),
+    path('subprogram_add/<int:program>', SubprogramCreation.as_view(), name='subprogram_add'),
 ]
--- programs/views.py
+++ programs/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Program, SubProgram, ProgramTask, TaskAdvance
@@ -27,11 +27,40 @@ class ProgramUpdate(LoginRequiredMixin, UpdateView):
     fields = ['name', 'description', 'start_date', 'end_date']
     success_url = reverse_lazy('program_list')
 
+class ProgramDetail(LoginRequiredMixin, DetailView):
+    model = Program
+    template_name = "programs/program_detail.html"
+    login_url = LOGIN_URL
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['subprograms'] = SubProgram.objects.filter(program=kwargs['object'])
+        return data
+
 class SubProgramList(LoginRequiredMixin, ListView):
     model = SubProgram
     template_name = "programs/subprograms_list.html"
     paginate_by = 10
     login_url = LOGIN_URL
+
+class SubprogramCreation(LoginRequiredMixin, CreateView):
+    model = SubProgram
+    template_name = "programs/subprogram_add.html"
+    fields = ['name', 'description', 'start_date', 'end_date']
+    login_url = LOGIN_URL
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['program'] = Program.objects.get(pk=self.kwargs['program'])
+        return data
+    
+    def form_valid(self, form):
+        form.instance.program = Program.objects.get(pk=self.kwargs['program'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        print(self.kwargs)
+        return reverse_lazy('program_detail', kwargs={'pk': self.kwargs["program"]})
 
 
 
